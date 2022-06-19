eaa6985aecd2bab91a0502ec4c2216d413f1740e
Paul Firtala <paul.firtala@roinspace.com>
2022-06-19 20:39:29 +0300

    Added Makefile. Added <chronos>. Works well

--- SIFT_Cpp/src/match_features.cpp
+++ SIFT_Cpp/src/match_features.cpp
@@ -1,8 +1,11 @@
 #include <iostream> 
 #include <string>
+#include <chrono>
 
 #include "image.hpp"
 #include "sift.hpp"
+
+using namespace std::chrono;
 
 int main(int argc, char *argv[])
 {
@@ -17,9 +20,15 @@ int main(int argc, char *argv[])
     a = a.channels == 1 ? a : rgb_to_grayscale(a);
     b = b.channels == 1 ? b : rgb_to_grayscale(b);
 
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::vector<sift::Keypoint> kps_a = sift::find_keypoints_and_descriptors(a);
     std::vector<sift::Keypoint> kps_b = sift::find_keypoints_and_descriptors(b);
     std::vector<std::pair<int, int>> matches = sift::find_keypoint_matches(kps_a, kps_b);
+
+    auto stop = std::chrono::high_resolution_clock::now();
+    std::cout << "Done in " << duration_cast<std::chrono::milliseconds>(stop-start).count() << " ms" << std::endl;
+    
     Image result = sift::draw_matches(a, b, kps_a, kps_b, matches);
     result.save("result.jpg");
     
